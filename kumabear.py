f71209b07ee981e907ec3d397920b0bc28c8188b
ColaZZ <zlinxx@foxmail.com>
2017-03-22 14:29:12 +0800

    POST表单的响应改为重定向,name保存在session中,两次请求中间也能记住name
    POST表单的响应改为重定向,name保存在session中,两次请求中间也能记住name

--- kumabear.py
+++ kumabear.py
@@ -1,9 +1,9 @@
 # coding=utf-8
-from flask import Flask, render_template
-from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
-from flask.ext.wtf import Form
+from flask import Flask, render_template, session, redirect, url_for
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
@@ -17,19 +17,18 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-class NameForm(Form):
+class NameForm(FlaskForm):
 	name = StringField(u'你的名字是?', validators=[Required()])
 	submit = SubmitField(u'提交')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.errorhandler(404)
 def page_not_found(e):
