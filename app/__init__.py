e41e95de8f7d480ca6a7270a641fac255e1c164a
ColaZZ <zlinxx@foxmail.com>
2017-03-23 17:26:51 +0800

    使用Flask-Login认证登录用户
    登录,登出,认证

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,15 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 # from flask_mail import Mail, Message
 from config import config
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 # mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -19,6 +23,7 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     moment.init_app(app)
+    login_manager.init_app(app)
     db.init_app(app)
 
     # 附加路由器和自定义错误页面
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,25 @@
 # coding=utf-8
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required
 from . import auth
+from ..models import User
+from .forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit:
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash(u'非法的用户或密码')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    loggout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
