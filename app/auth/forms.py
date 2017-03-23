f10a9c0447bed4c3ec29ddf5b583e6b2340ae27c
ColaZZ <zlinxx@foxmail.com>
2017-03-24 00:23:52 +0800

    注册新用户
    Register

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,12 +1,34 @@
 # coding=utf-8
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 
 class LoginForm(Form):
-    email = StringField(u'邮件地址', validators=[Required(), Length(1, 64),
-                                             Email()])
-    password = PasswordField(u'密码', validators=[Required()])
+    email = StringField(u'邮件地址', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField(u'密码', validators=[DataRequired()])
     remember_me = BooleanField(u'记住我(保持登录)')
     submit = SubmitField(u'登录')
+
+
+class RegistrationForm(Form):
+    email = StringField(u'邮件地址', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField(u'用户名', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                                                     'Username must have one letters',
+                                                                                     'numbers, dots or uderscores')])
+    password = PasswordField(u'密码', validators=[
+        DataRequired(), Length(1, 64), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField(u'确认密码', validators=[DataRequired()])
+    submit = SubmitField(u'注册')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validator_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already registered.')
+
+
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -20,6 +22,20 @@ def login():
 @auth.route('/logout')
 @login_required
 def logout():
-    loggout_user()
+    logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html')
+
+
+
