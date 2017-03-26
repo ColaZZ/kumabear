c42469ae9bf998bd1429df2e8b5c2704352e4d82
ColaZZ <zlinxx@foxmail.com>
2017-03-26 12:00:51 +0800

    用户头像
    user avatar

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from datetime import datetime
+import hashlib
+from flask import request
 
 
 class Permission:
@@ -144,6 +146,15 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, dafault=default, rating=rating)
 
     def __repr__(self):
         return '<User %r>' % self.username
